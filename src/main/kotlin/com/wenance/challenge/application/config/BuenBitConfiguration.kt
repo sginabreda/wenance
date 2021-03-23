22ff225613cd50d575c6140f9a4b15c0c302facc
SANTIAGO GINABREDA <sginabreda@santandertecnologia.com.ar>
2021-03-22 22:02:13 -0300

    Config files

--- src/main/kotlin/com/wenance/challenge/application/config/BuenBitConfiguration.kt
+++ src/main/kotlin/com/wenance/challenge/application/config/BuenBitConfiguration.kt
@@ -1,20 +1,36 @@
 package com.wenance.challenge.application.config
 
+import io.netty.channel.ChannelOption
+import io.netty.handler.ssl.SslContext
+import io.netty.handler.ssl.SslContextBuilder
+import io.netty.handler.timeout.ReadTimeoutHandler
+import io.netty.handler.timeout.WriteTimeoutHandler
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.Connection
+import reactor.netty.http.client.HttpClient
+import reactor.netty.tcp.TcpClient
 import java.util.Collections
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class BuenBitConfiguration {
+
+    private val timeout = 10000L
 
     @Bean
     fun buenBitWebClient(
         @Value("\${services.buenBit.host}") host: String
     ): WebClient {
+
+        val writeTimeOut = 4000L
+        val readTimeOut = 3000L
+        val tcpClient = trustedConnection(createSslContext(), readTimeOut, writeTimeOut)
 
         return WebClient
             .builder()
@@ -28,7 +44,33 @@ class BuenBitConfiguration {
                             )
                 )
             }
+            .clientConnector(ReactorClientHttpConnector(HttpClient.from(tcpClient).followRedirect(true)))
             .defaultUriVariables(Collections.singletonMap("url", host))
             .build()
+    }
+
+    fun createSslContext(): SslContext {
+        return SslContextBuilder.forClient()
+            .sslProvider(io.netty.handler.ssl.SslProvider.JDK)
+            .build()
+    }
+
+    fun trustedConnection(
+        sslContext: SslContext,
+        readTimeout: Long = timeout,
+        writeTimeout: Long = timeout
+    ): TcpClient {
+        return connection(readTimeout, writeTimeout)
+            .secure { sslContextSpec -> sslContextSpec.sslContext(sslContext) }
+    }
+
+    private fun connection(readTimeout: Long, writeTimeout: Long): TcpClient {
+        return TcpClient
+            .create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeout.toInt())
+            .doOnConnected { connection: Connection ->
+                connection.addHandlerLast(ReadTimeoutHandler(readTimeout, TimeUnit.MILLISECONDS))
+                connection.addHandlerLast(WriteTimeoutHandler(writeTimeout, TimeUnit.MILLISECONDS))
+            }
     }
 }
--- src/main/kotlin/com/wenance/challenge/application/config/MongoConfiguration.kt
+++ src/main/kotlin/com/wenance/challenge/application/config/MongoConfiguration.kt
@@ -2,27 +2,21 @@ package com.wenance.challenge.application.config
 
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
-import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoClients
+import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.core.MongoTemplate
 
 @Configuration
 class MongoConfiguration {
 
     @Bean
     fun mongoClient(): MongoClient {
-        val connection = ConnectionString("mongodb://localhost:27017/wenance")
+        val connection = ConnectionString("mongodb://localhost:27017/test")
         val mongoClientSettings = MongoClientSettings.builder()
             .applyConnectionString(connection)
             .build()
 
         return MongoClients.create(mongoClientSettings)
-    }
-
-    @Bean
-    fun mongoTemplate(): MongoTemplate {
-        return MongoTemplate(mongoClient(), "wenance")
     }
 }
